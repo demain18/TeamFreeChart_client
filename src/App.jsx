0b8858a93695913fa0631a214dc3974ac9b3dd96
Kimuhjin <ajin950714@naver.com>
2020-09-14 18:59:36 +0900

    [추가] : commom폴더, style.js 생성 - common파일과 style.js를 생성하여 버튼컴포넌트 추가하였습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { Fragment } from "react";
+import styled from "styled-components";
+import * as S from "./common/style";
 
 function App() {
-  return <div className="App">ssdfsdfds</div>;
+  return (
+    <Fragment>
+      <div className="App">
+        <Box>
+          <S.SmallButton_G>프로필 보기</S.SmallButton_G>
+          <S.Button_G>프로필 보기</S.Button_G>
+          <S.SmallButton_B>프로필 보기</S.SmallButton_B>
+          <S.Button_B>프로필 보기</S.Button_B>
+          <S.BigButton_W>조사 참여하기</S.BigButton_W>
+          <S.SnsButton>
+            <S.FaceBookIcon />
+            페이스북으로 시작
+          </S.SnsButton>
+        </Box>
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
+const Box = styled.div`
+  margin-top: 100px;
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+`;
---
+++ src/common/style.js
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+import { AiFillFacebook } from "react-icons/ai";
+
+export const FaceBookIcon = styled(AiFillFacebook)`
+  width: 18px;
+  height: 18px;
+`;
+export const SmallButton_G = styled.button`
+  cursor: pointer;
+  font-size: 13px;
+  font-weight: bold;
+  color: #000;
+  width: 96px;
+  height: 32px;
+  border-radius: 5px;
+  border: none;
+  box-sizing: border-box;
+  background-color: #e0e0e0;
+  &:hover {
+    background-color: #c4c4c4;
+  }
+`;
+export const SmallButton_B = styled.button`
+  cursor: pointer;
+  font-size: 13px;
+  font-weight: bold;
+  color: #fff;
+  width: 96px;
+  height: 32px;
+  border-radius: 5px;
+  border: none;
+  box-sizing: border-box;
+  background-color: #589fff;
+  &:hover {
+    background-color: #4581d3;
+  }
+`;
+
+export const Button_G = styled.button`
+  cursor: pointer;
+  font-size: 15px;
+  font-weight: bold;
+  color: #000;
+  width: 104px;
+  height: 40px;
+  border-radius: 5px;
+  border: none;
+  box-sizing: border-box;
+  background-color: #e0e0e0;
+  &:hover {
+    background-color: #c4c4c4;
+  }
+`;
+
+export const Button_B = styled.button`
+  cursor: pointer;
+  font-size: 15px;
+  font-weight: bold;
+  color: #fff;
+  width: 104px;
+  height: 40px;
+  border-radius: 5px;
+  border: none;
+  box-sizing: border-box;
+  background-color: #589fff;
+  &:hover {
+    background-color: #4581d3;
+  }
+`;
+
+export const BigButton_W = styled.button`
+  font-size: 18px;
+  width: 160px;
+  height: 40px;
+  border-radius: 5px;
+  border: solid 1px #d6d6d6;
+  background-color: #ffffff;
+`;
+
+export const SnsButton = styled.button`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border: none;
+  padding: 0px 20px;
+  width: 176px;
+  height: 40px;
+  font-size: 15px;
+  color: #fff;
+  font-weight: bold;
+  border-radius: 5px;
+  background-color: #3b5998;
+  /* background-color: ${(props) => props.color || "white"}; */
+`;
