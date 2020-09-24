23309f2f6d69fb37af242f9d5489ccaaff1d8ee7
Kimuhjin <ajin950714@naver.com>
2020-09-25 01:38:04 +0900

    [추가] : MainWageArea/InfoArea 생성,  - common파일과 style.js를 생성하였습니다.

--- src/components/Home/MainWageArea/InfoArea/index.jsx
+++ src/components/Home/MainWageArea/InfoArea/index.jsx
@@ -3,7 +3,24 @@ import * as S from "components/Home/MainWageArea/InfoArea/style";
 function index() {
   return (
     <Fragment>
-      <S.Layout></S.Layout>
+      <S.Layout>
+        <S.SelectWrapper>
+          <S.Select>프로그래밍</S.Select>
+          <S.Select>프론트엔드</S.Select>
+        </S.SelectWrapper>
+        <S.InfoArea>
+          <S.InfoTitle>웹 프론트엔드 개발 평균급여</S.InfoTitle>
+          <S.InfoWageArea>
+            <S.InfoWageValue>350만원</S.InfoWageValue>
+            <S.InfoWageValue className="month">/1개월</S.InfoWageValue>
+          </S.InfoWageArea>
+          <S.InfoButton>조사 참여하기</S.InfoButton>
+        </S.InfoArea>
+        <S.InfoMore>
+          해당 자료는 현업에 종사하시는 분들의 정보 제공으로 구성{"\n"}되어
+          있으며 매주 금요일 오후 6시마다 갱신됩니다.
+        </S.InfoMore>
+      </S.Layout>
     </Fragment>
   );
 }
--- src/components/Home/MainWageArea/InfoArea/style.js
+++ src/components/Home/MainWageArea/InfoArea/style.js
@@ -3,10 +3,81 @@ import styled from "styled-components";
 export const Layout = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: space-between;
+  justify-content: space-between;
+  align-items: flex-start;
   width: 352px;
   height: 360px;
-  background-color: #f2f2f2;
-  opacity: 0.2;
+  /* background-color: #f2f2f2;
+  opacity: 0.2; */
+`;
+
+export const SelectWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const Select = styled.div`
+  cursor: pointer;
+  display: flex;
+  padding: 0px 10px;
+  justify-content: flex-start;
+  align-items: center;
+  font-size: 15px;
+  color: #1d1d1d;
+  width: 170px;
+  height: 38px;
+  border-radius: 3px;
+  border: solid 1px #e0e0e0;
+  background-color: #ffffff;
+`;
+export const InfoArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  width: 100%;
+  height: 144px;
+  /* border: 1px solid red; */
+  box-sizing: border-box;
+`;
+
+export const InfoTitle = styled.div`
+  font-size: 18px;
+  color: #ffffff;
+`;
+
+export const InfoWageArea = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-end;
+`;
+export const InfoWageValue = styled.div`
+  color: #fff;
+  font-weight: bold;
+  font-size: 50px;
+  &.month {
+    font-size: 18px;
+    font-weight: normal;
+    margin-bottom: 10px;
+  }
+`;
+
+export const InfoButton = styled.button`
+  cursor: pointer;
+  outline: none;
+  width: 139px;
+  height: 43px;
+  border-radius: 5px;
+  border: solid 1px #d6d6d6;
+  background-color: #ffffff;
+  font-size: 18px;
+  color: #141414;
+`;
+
+export const InfoMore = styled.div`
+  width: 100%;
+  height: 40px;
+  font-size: 15px;
+  color: #ffffff;
 `;
