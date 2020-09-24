64e24c65800f3c8bdf42f6606604a52f1f0870ca
Kimuhjin <ajin950714@naver.com>
2020-09-25 01:42:28 +0900

    Updates

---
+++ precache-manifest.5065b1c4321faba931911614e43347fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8830c9995c0dc412eabd5316496013b8",
+    "url": "/TeamFreeChart_client/index.html"
+  },
+  {
+    "revision": "2f948eedd0f301e92ab8",
+    "url": "/TeamFreeChart_client/static/js/2.6f25acd6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TeamFreeChart_client/static/js/2.6f25acd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f288d2c252772b660e73",
+    "url": "/TeamFreeChart_client/static/js/main.6715c33e.chunk.js"
+  },
+  {
+    "revision": "0f6f93d0644867202437",
+    "url": "/TeamFreeChart_client/static/js/runtime-main.07eb250e.js"
+  }
+]);
