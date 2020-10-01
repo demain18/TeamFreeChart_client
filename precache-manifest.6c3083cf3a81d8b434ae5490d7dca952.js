446cd305322b6dbcd37fa4b4ea4eb82d7c31b522
Kimuhjin <ajin950714@naver.com>
2020-10-01 16:46:14 +0900

    Updates

---
+++ precache-manifest.6c3083cf3a81d8b434ae5490d7dca952.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd7bdfcf6aabebe8dd6b11331ae815ed",
+    "url": "/TeamFreeChart_client/index.html"
+  },
+  {
+    "revision": "d797c1336537c685b5f9",
+    "url": "/TeamFreeChart_client/static/css/main.f4b16a74.chunk.css"
+  },
+  {
+    "revision": "6f9cd9477add95d8dc13",
+    "url": "/TeamFreeChart_client/static/js/2.7723b9fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TeamFreeChart_client/static/js/2.7723b9fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a28736418e8b095e1e60",
+    "url": "/TeamFreeChart_client/static/js/3.65996edc.chunk.js"
+  },
+  {
+    "revision": "33d1dc969d1bfdab5b8c",
+    "url": "/TeamFreeChart_client/static/js/4.4742ffdf.chunk.js"
+  },
+  {
+    "revision": "50807a3c8c0c1073e1c8",
+    "url": "/TeamFreeChart_client/static/js/5.b66e22ce.chunk.js"
+  },
+  {
+    "revision": "d797c1336537c685b5f9",
+    "url": "/TeamFreeChart_client/static/js/main.86f52e79.chunk.js"
+  },
+  {
+    "revision": "a841a2a752eabe2e92af",
+    "url": "/TeamFreeChart_client/static/js/runtime-main.ebd06838.js"
+  },
+  {
+    "revision": "1968362aef735c258f1a90901bbd043e",
+    "url": "/TeamFreeChart_client/static/media/NanumGothic.1968362a.woff2"
+  },
+  {
+    "revision": "380b4884f385e4f5736b14649886f8fe",
+    "url": "/TeamFreeChart_client/static/media/NanumGothic.380b4884.woff"
+  },
+  {
+    "revision": "66b97348d857f6a16bbf3bc1c46ea169",
+    "url": "/TeamFreeChart_client/static/media/NanumGothic.66b97348.ttf"
+  },
+  {
+    "revision": "b8c7d139b3e870af0e27848007d1d66b",
+    "url": "/TeamFreeChart_client/static/media/NanumGothic.b8c7d139.eot"
+  },
+  {
+    "revision": "404c8be6878d9b2012db07ade804e6f0",
+    "url": "/TeamFreeChart_client/static/media/NanumGothicBold.404c8be6.woff2"
+  },
+  {
+    "revision": "5da44771dc94824508cfa1a13240e0a5",
+    "url": "/TeamFreeChart_client/static/media/NanumGothicBold.5da44771.ttf"
+  },
+  {
+    "revision": "63ab6407fb7fb62e71b85941c6bc571d",
+    "url": "/TeamFreeChart_client/static/media/NanumGothicBold.63ab6407.woff"
+  },
+  {
+    "revision": "8c3d6febfdedbd54887252e3c92544a6",
+    "url": "/TeamFreeChart_client/static/media/NanumGothicBold.8c3d6feb.eot"
+  },
+  {
+    "revision": "1a9b326e1a15e96ce2a95e4a10ea6302",
+    "url": "/TeamFreeChart_client/static/media/NanumGothicExtraBold.1a9b326e.woff2"
+  },
+  {
+    "revision": "46e34f91d74113af341015348e711391",
+    "url": "/TeamFreeChart_client/static/media/NanumGothicExtraBold.46e34f91.eot"
+  },
+  {
+    "revision": "b91a24fe1ae5ce3b9b59f3b410f7239a",
+    "url": "/TeamFreeChart_client/static/media/NanumGothicExtraBold.b91a24fe.woff"
+  },
+  {
+    "revision": "f47cdac8642ac173cdf38dc500fe9eb4",
+    "url": "/TeamFreeChart_client/static/media/NanumGothicExtraBold.f47cdac8.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TeamFreeChart_client/precache-manifest.c63796b047f2430699e1cd801ec2ff95.js"
+  "/TeamFreeChart_client/precache-manifest.6c3083cf3a81d8b434ae5490d7dca952.js"
 );
 
 self.addEventListener('message', (event) => {
