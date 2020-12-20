8817958e1436f208a2a9c5860641296b3d508b2f
jihee <dhkdnun@naver.com>
2020-12-20 22:44:20 +0900

    [추가] : GallertWritePost 생성 - 갤러리 글 쓰기 화면 퍼블 작업

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,14 @@ import { Route } from "react-router-dom";
 const Home = lazy(() => import("pages/Home"));
 const GalleryExplore = lazy(() => import("pages/GalleryExplore"));
 const GalleryDetail = lazy(() => import("pages/GalleryDetail"));
+const GalleryWritePost = lazy(() => import("pages/GalleryWritePost"));
 function Router() {
   return (
     <>
       <Route exact path="/" component={Home} />
       <Route exact path="/GalleryExplore" component={GalleryExplore} />
       <Route exact path="/GalleryDetail" component={GalleryDetail} />
+      <Route exact path="/GalleryWritePost" component={GalleryWritePost} />
     </>
   );
 }
---
+++ src/components/GalleryWritePost/InfoArea/style.js
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+import { AiFillCaretDown } from "react-icons/ai";
+
+export const Layout = styled.div`
+  display: flex;
+  margin-top : 15px; 
+`;
+
+
+export const Select = styled.div`
+  cursor: pointer;
+  outline: none;
+  display: flex;
+  font-size: 15px;
+  color: #767676;
+  padding: 0px 10px;
+  width: 100%;
+  margin-top:5px;
+  height: 38px;
+  border-radius: 3px;
+  border: solid 1px #e0e0e0;
+  background-color: #ffffff;
+  justify-content: space-between;
+  align-items: center;
+
+  box-sizing: border-box;
+`;
+
+export const DownTriIcon = styled(AiFillCaretDown)`
+  width: 15px;
+  height: 15px;
+  color: #000;
+`;
+
+export const CommentInput = styled.textarea`
+  resize: none;
+  margin-top: 10px;
+  width: 100%;
+  height: 250px;
+  padding: 15px;
+  border: solid 1px #d6d6d6;
+  box-sizing: border-box;
+  &::placeholder {
+    font-size: 15px;
+    color: #b5b5b5;
+  }
+`;
+
+export const InputPeriod = styled.input`
+  width: 80%;
+  height: 5px;
+  padding: 15px;
+  margin-top:5px;
+  border: solid 1px #d6d6d6;
+`;
+
+
+export const SelectDiv = styled.div`
+  width: 130px;
+  margin-right: 10px;
+`;
+
+export const InputCheckbox = styled.input.attrs({
+  type:'checkbox',
+})`
+  width: 20px;
+  height: 20px;
+`;
+
+export const CheckboxDiv = styled.div`
+  margin-top: 30px;
+`;
